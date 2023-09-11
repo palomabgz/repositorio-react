d768c12824c869085f9198acf8e92d8679b30372
palomabgz <palomabgz@gmail.com>
2023-09-11 18:41:06 -0300

    modificación

--- segundo-proyecto/src/main.jsx
+++ segundo-proyecto/src/main.jsx
@@ -1,37 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './App.jsx'
+import App from './App.jsx'
 import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
-
-root.render(
-  <App />
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
-
-
-
-
-// ---------------- EJEMPLO BOTÓN:
-// const ButtonCustom = ({text}) => {
-//   return(
-//     <button>
-//       {text}
-//     </button>
-//   )
-// }
-
-// root.render(
-//   <React.Fragment>
-//     <ButtonCustom text='Me gusta'/>
-//   </React.Fragment>
-// )
-
-// ---------------- ACLARACIONES: 
-// React: Son funciones que entregan objetos (componentes).
-// PascalCase = React.
-// camelCase = JavaScript.
-// snake_case.
-// kebab-case.
-// Para React Native se necesita añadir el CSS directamente en la etiqueta de esta forma: ejemplo > "style={{ display: 'flex'}}"
-// <React.Fragment> es para cuando se desea envolver dos o más elementos en React y poder renderizarlos (puede utilizarse <> y </> también)
