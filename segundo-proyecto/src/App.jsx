0c2153931ed6337728b65413d7c7c9c95786c504
palomabgz <palomabgz@gmail.com>
2023-09-11 14:53:18 -0300

    creación

---
+++ segundo-proyecto/src/App.jsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+
+import { TwitterCardContainer } from './componentes/TwitterCard/TwitterCardContainer'
+
+export function App () {
+  return (
+    <section className="App">
+      <TwitterCardContainer/>
+    </section>
+  )
+}
